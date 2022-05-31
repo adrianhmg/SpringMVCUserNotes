0a01f287bce6dc9c92a5bc778c7ee1fb287212ce
Adrian Moreno <Adrian_Moreno@epam.com>
2022-05-30 19:39:47 -0500

    First Commit

---
+++ src/main/java/com/example/SpringRestUserNotes/SpringRestUserNotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringRestUserNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestUserNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestUserNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringRestUserNotes/SpringRestUserNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringRestUserNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestUserNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
